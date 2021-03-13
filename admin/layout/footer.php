dc5f6962689c71e0e54a697859a213b54fea78dc
Vishwanath Hegde <vishwahegde27@gmail.com>
2021-03-13 11:36:12 +0530

    Blog

---
+++ admin/layout/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.0.0
+    </div>
+    <strong>Copyright &copy; 2021 <a href="https://firetechie.ml" target="_blank">Fire Techie</a>.</strong> All rights
+    reserved.
+  </footer>
---
+++ layout/footer.php
@@ -0,0 +1,6 @@
+<footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; <a href="https://firetechie.ml" target="_blank"> Fire Techie 2021</a></p>
+    </div>
+    <!-- /.container -->
+  </footer>
